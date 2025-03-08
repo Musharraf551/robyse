9f6c2202699369068ec2daa24029defee858c4b9
musharraf <musharrafaleem2@gmail.com>
2025-03-08 11:59:51 +0530

    created queryset for both models in views

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html')
+    cate= ItemList.objects.all()
+    item = Items.objects.all()
+    context = {'cate':cate, 'item':item}
+    return render(request, 'home.html', context)
