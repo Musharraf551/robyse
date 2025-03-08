2ec3f97c03868661707ee18c2d52094db36adcea
musharraf <musharrafaleem2@gmail.com>
2025-03-08 11:23:51 +0530

    registered static in settings.py

--- robyse/settings.py
+++ robyse/settings.py
@@ -18,6 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATE_DIR=os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -119,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
