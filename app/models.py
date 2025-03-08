7918f72eee3dbe11050ceb4d3fe78e63a4239ed2
musharraf <musharrafaleem2@gmail.com>
2025-03-08 14:29:23 +0530

    added menu section in home

--- app/models.py
+++ app/models.py
@@ -9,10 +9,11 @@ class ItemList(models.Model):
     
 class Items(models.Model):
     Item_name = models.CharField(max_length=40)
-    description = models.TextField(blank=False)
+    
     Price = models.IntegerField()
     Category = models.ForeignKey(ItemList, related_name='Name', on_delete=models.CASCADE)
     Image = models.ImageField(upload_to='items/')
     
     def __str__(self):
         return self.Item_name
+
