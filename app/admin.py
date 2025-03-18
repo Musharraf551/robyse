f7090eb953d9c198d7b9de448b59f49d711d8c68
musharraf <musharrafaleem2@gmail.com>
2025-03-18 16:32:49 +0530

    registered the category model in admin

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 from app.models import *
 # Register your models here.
 
-admin.site.register(ItemList)
-admin.site.register(Items)
-admin.site.register(More)
+admin.site.register(Category)
