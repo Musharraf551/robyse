96f8d674e4d5a91344a84a4108bc2c9559761530
musharraf <musharrafaleem2@gmail.com>
2025-03-18 16:39:39 +0530

    modified the home view

--- app/views.py
+++ app/views.py
@@ -3,15 +3,6 @@ from app.models import *
 from django.views.generic import DetailView
 # Create your views here.
 def home_view(request):
-    cate= ItemList.objects.all()
-    item = Items.objects.all()
-    context = {'cate':cate, 'item':item}
-    return render(request, 'home.html', context)
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'categories': categories})
 
-class Items_Detail(DetailView):
-    model=More
-    context_object_name='moreobj'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['more'] = self.object.More.all()  # Ensure students are retrieved
-        return context
