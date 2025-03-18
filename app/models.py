551e4248ffdea39ebf8aff76229215aeb3b584fe
musharraf <musharrafaleem2@gmail.com>
2025-03-18 16:37:12 +0530

    added a description variable in category model

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
-
+    description = models.CharField(max_length=200)
+    
     def __str__(self):
         return self.name
