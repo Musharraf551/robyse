16b1ff2b3d731a9c2378704f6f45ff0306b92756
musharraf <musharrafaleem2@gmail.com>
2025-03-18 16:19:04 +0530

    removed nested app folder

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class ItemList(models.Model):
@@ -8,11 +9,13 @@ class ItemList(models.Model):
         return self.Category_name
     
 class Items(models.Model):
-    Item_name = models.CharField(max_length=40)
-    
+    Item_name = models.CharField(max_length=40)   
     Price = models.IntegerField()
     Category = models.ForeignKey(ItemList, related_name='Name', on_delete=models.CASCADE)
     Image = models.ImageField(upload_to='items/')
+    
+    def get_absolute_url(self):
+        return reverse("more_detail",kwargs={'pk':self.pk})
     
     def __str__(self):
         return self.Item_name
--- robyse/urls.py
+++ robyse/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from app.views import *
@@ -23,6 +23,8 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home_view/',home_view, name='home_view'),
+    re_path('(?P<pk>\d+)/',Items_Detail.as_view(),name='Items_Detail'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
