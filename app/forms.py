9358a4f9fbd55ba01ad1f6b4c28acdb1a05651a9
musharraf <musharrafaleem2@gmail.com>
2025-03-19 14:36:11 +0530

    created model form for feedbackform in forms.py

--- app/forms.py
+++ app/forms.py
@@ -10,3 +10,15 @@ class BookingForm(forms.ModelForm):
             'time_slot': forms.TextInput(attrs={'placeholder': 'e.g., 10:00 AM - 12:00 PM'}),
             'number_of_people': forms.NumberInput(attrs={'min': 1}),
         }
+        
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['reviewer_name','date','comment','rating']
+        widgets = {
+            'reviewer_name': forms.TextInput(attrs={'type': 'text'}),
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            
+            'comment': forms.TextInput(attrs={'type': 'text'}),
+            'rating': forms.NumberInput(attrs={'min': 1}),    
+        }
