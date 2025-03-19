386db90324f6ca341806a974c847c30c3b334f79
musharraf <musharrafaleem2@gmail.com>
2025-03-19 12:01:24 +0530

    created view for instance detail

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from app.models import *
 from django.views.generic import DetailView
 # Create your views here.
@@ -7,3 +7,7 @@ def home_view(request):
     instances = Model1.objects.all()
     return render(request, 'home.html', {'categories': categories, 'instances': instances})
 
+def instance_detail(request, pk):
+    instance = get_object_or_404(Model1, pk=pk)
+    reviews = instance.reviews.all()
+    return render(request, 'instance_detail.html', {'instance': instance, 'reviews':reviews})
