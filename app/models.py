1e69216fffc90ffb95e127edc26d1311b0cfee40
musharraf <musharrafaleem2@gmail.com>
2025-03-19 11:13:20 +0530

    created a model named Model1

--- app/models.py
+++ app/models.py
@@ -8,3 +8,16 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Model1(models.Model):
+    category= models.ForeignKey(Category, on_delete=models.CASCADE, related_name='model1_items')
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    location = models.CharField(max_length=255)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
