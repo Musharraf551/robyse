460c74c10cf23f51cc30708e028fdcdf82c15299
musharraf <musharrafaleem2@gmail.com>
2025-03-19 11:16:18 +0530

    modified home view

--- app/views.py
+++ app/views.py
@@ -4,5 +4,6 @@ from django.views.generic import DetailView
 # Create your views here.
 def home_view(request):
     categories = Category.objects.all()
-    return render(request, 'home.html', {'categories': categories})
+    instances = Model1.objects.all()
+    return render(request, 'home.html', {'categories': categories, 'instances': instances})
 
