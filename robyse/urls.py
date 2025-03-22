4ea6f58e272e767a74d0e5ba4db4786e201aa9a8
musharraf <musharrafaleem2@gmail.com>
2025-03-22 18:03:10 +0530

    done url mapping for dashboard

--- robyse/urls.py
+++ robyse/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', signup, name='signup'),
+    path('dashboard/', dashboard, name='dashboard'),
     
 
 ]
