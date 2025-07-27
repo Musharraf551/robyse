a1c4f390e02345fa314db9bcfdaadad3cdc3fcad
Musharraf551 <musharrafaleem2@gmail.com>
2025-07-27 17:37:06 +0530

    Update settings.py

--- robyse/settings.py
+++ robyse/settings.py
@@ -144,8 +144,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'av1161063@gmail.com'  
-EMAIL_HOST_PASSWORD = 'hpjh aval obxl mugm' 
+EMAIL_HOST_USER = 'yourgmail@gmail.com'  
+#create your own app password
+EMAIL_HOST_PASSWORD = 'xxxx xxxx xxxx xxxx' 
 
 
 # Default primary key field type
